31f69d2ffc45cbe48e98a9dc05b63b4881fcd849
spring-backend-mobdev <enieto@mobdev.cl>
2022-02-19 03:03:55 -0300

    Initial commit

---
+++ src/main/java/cl/challenge/javacoding/BookJavaCodingProblemsApplication.java
@@ -0,0 +1,13 @@
+package cl.challenge.javacoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJavaCodingProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookJavaCodingProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/challenge/javacoding/BookJavaCodingProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.challenge.javacoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJavaCodingProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
